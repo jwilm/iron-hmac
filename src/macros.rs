13fe5c9b29baa398e1e65e8d6d09efd417942226
Joe Wilm <joe@jwilm.com>
2015-12-13 15:09:33 -0800

    Remove unused print_hex! macro

--- src/macros.rs
+++ src/macros.rs
@@ -11,10 +11,3 @@ macro_rules! forbidden {
         return Err(::iron::IronError::new($err, ::iron::status::Forbidden));
     }};
 }
-
-/// Serialize a &[u8] sequence and pass it to println with the provided format string.
-macro_rules! print_hex {
-    ($fmt_str:expr, $bytes:expr) => {{
-        println!($fmt_str, ::util::to_hex($bytes));
-    }}
-}
